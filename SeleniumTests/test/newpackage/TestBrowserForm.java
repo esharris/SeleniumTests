28e85ab18ac8978f1c9b181bf7a8d7e0423db03a
Earl Harris <esharris@mac.com>
2023-06-29 10:53:43 -0400

    Added more tessts to TestBrowserForm.

--- SeleniumTests/test/newpackage/TestBrowserForm.java
+++ SeleniumTests/test/newpackage/TestBrowserForm.java
@@ -2,6 +2,8 @@ package newpackage;
 
 import static org.junit.Assert.assertEquals;
 
+import java.time.Duration;
+
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -13,6 +15,8 @@ public abstract class TestBrowserForm extends TestBrowserBase {
 	private static final String EMAIL_INPUT = "abcd@gmail.com";
 	private static final String PASSWORD_INPUT = "abcdefghlkjl";
 
+	private static final String EXPECTED_OUTPUT = "Successfully Logged in...";
+
 	public TestBrowserForm() {
 		super();
 	}
@@ -21,16 +25,58 @@ public abstract class TestBrowserForm extends TestBrowserBase {
 	public void testForm() {
 		driver.get(BASE_URL);
 
-		// Get the WebElement corresponding to the Email Address(TextField)
-		WebElement email = driver.findElement(By.id("email"));
-
-		// Get the WebElement corresponding to the Password Field
-		WebElement password = driver.findElement(By.name("passwd"));
+		final WebElement email = driver.findElement(By.id("email"));
+		final WebElement password = driver.findElement(By.name("passwd"));
 
 		email.sendKeys(EMAIL_INPUT);
 		password.sendKeys(PASSWORD_INPUT);
 
 		assertEquals(EMAIL_INPUT, email.getAttribute("value"));
 		assertEquals(PASSWORD_INPUT, password.getAttribute("value"));
+
+		email.clear();
+		password.clear();
+		assertEquals("", email.getAttribute("value"));
+		assertEquals("", password.getAttribute("value"));
+
+	}
+
+	/**
+	 * Login Done with Click
+	 */
+	@Test
+	public void testFormClick() {
+		driver.get(BASE_URL);
+
+		final WebElement email = driver.findElement(By.id("email"));
+		final WebElement password = driver.findElement(By.name("passwd"));
+
+		final WebElement login = driver.findElement(By.id("SubmitLogin"));
+
+		email.sendKeys(EMAIL_INPUT);
+		password.sendKeys(PASSWORD_INPUT);
+		login.click();
+
+		assertEquals(EXPECTED_OUTPUT, driver.findElement(By.className("error-copy")).getText());
+
+	}
+
+	/**
+	 * Login Done with Submit
+	 */
+	@Test
+	public void testFormSubmit() {
+
+		driver.get(BASE_URL);
+		driver.findElement(By.id("email")).sendKeys(EMAIL_INPUT);
+		driver.findElement(By.name("passwd")).sendKeys(PASSWORD_INPUT);
+		driver.findElement(By.id("SubmitLogin")).submit();
+
+		/**
+		 * Firefox requires a wait to work properly.
+		 */
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		assertEquals(EXPECTED_OUTPUT, driver.findElement(By.className("error-copy")).getText());
+
 	}
 }
